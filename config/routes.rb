1d5b336501ac6efab73726c98116777049cd0d4e
Vittorio Molaschi <vittorio.molaschi@gmail.com>
2018-04-10 16:47:16 +0200

    Working app without formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :tasks
+
+  # get "/", to: "tasks#index"
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # post "tasks", to: "tasks#create"
+  # get "tasks/new", to: "tasks#new"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
